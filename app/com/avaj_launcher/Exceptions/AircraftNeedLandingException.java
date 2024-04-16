3efbbdc4d67fab5f588cebeb88cfc6dfe6b81bea
coscialp <corentin.scialpi@gmail.com>
2024-04-16 18:30:29 +0200

    [Fix] remove unused exception

--- app/com/avaj_launcher/Exceptions/AircraftNeedLandingException.java
+++
@@ -1,7 +0,0 @@
-package com.avaj_launcher.Exceptions;
-
-public class AircraftNeedLandingException extends Exception {
-    public AircraftNeedLandingException() {
-        super("Aircraft need to land");
-    }
-}
